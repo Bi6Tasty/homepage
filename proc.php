c33b007450e6c551177d35736dd0bf6e312fd232
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-09 15:17:24 +0300

    Update proc.php

--- proc.php
+++ proc.php
@@ -3,9 +3,13 @@ $l = $_POST["login"];
 $p = $_POST["password"];
 
 include("cfg.php");
-$mysqli = new mysqli("SELECT * FROM users WHERE login=$l AND password=$P")
-
-echo "Hello user $l, Password used $p";
+$result = new mysqli("SELECT * FROM users WHERE login=$l AND password=$p");
+if (mysql_num_row($result) > 0){
+    echo "veri gud";
+}
+else{
+    echo "shit, try again":
+}
 
 ?>
 
