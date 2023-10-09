f1e699066421be6712be8712d07e3e2e1e1e058e
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-09 15:25:09 +0300

    Update proc.php

--- proc.php
+++ proc.php
@@ -3,8 +3,8 @@ $l = $_POST["login"];
 $p = $_POST["password"];
 
 include("cfg.php");
-$result = new mysqli("SELECT * FROM users WHERE login=$l AND password=$p");
-if (mysql_num_row($result) > 0){
+$result = $mysqli->query("SELECT * FROM users WHERE (login='$l') AND (password='$p')");
+if (mysqli_num_row($result) > 0){
     echo "veri gud";
 }
 else{
