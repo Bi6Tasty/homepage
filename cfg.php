3885e5389e47d9b85aed59303e64971f354909af
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-09 15:08:05 +0300

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -7,10 +7,3 @@ $db = "projekts2023";
 
 
 $mysqli = new mysqli($servername,$username,$password,$db);
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
