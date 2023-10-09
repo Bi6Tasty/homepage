472585f6cca59955aeb1074a1abb2d0b349fc2b4
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-09 15:12:14 +0300

    Update proc.php

--- proc.php
+++ proc.php
@@ -1,6 +1,10 @@
 <?php
 $l = $_POST["login"];
 $p = $_POST["password"];
+
+include("cfg.php");
+$mysqli = new mysqli("SELECT * FROM users WHERE login=$l and password")
+
 echo "Hello user $l, Password used $p";
 
 ?>
