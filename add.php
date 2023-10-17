f944275e68fe21df2488131b9b67e2995a41f498
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-17 16:02:22 +0300

    Update add.php

---
+++ add.php
@@ -0,0 +1,11 @@
+<?php
+
+$v = $_POST["vards"];
+$u = $_POST["uzvards"];
+$l = $_POST["login"];
+$p = md5($_POST["password"]);
+
+include("cfg.php");
+$mysqli->query("UPDATE `idk` SET `id`=null,`name`='$v',`sname`='$u',`login`='$l',`password`='$p'");
+
+?>
