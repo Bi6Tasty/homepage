60805bdcbc36cfeae1bb74dd148beb16a69dcaa7
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-17 15:53:14 +0300

    add user/password

--- proc.php
+++ proc.php
@@ -1,9 +1,9 @@
 <?php
 $l = $_POST["login"];
-$p = $_POST["password"];
+$p = md5($_POST["password"]);
 
 include("cfg.php");
-$result = $mysqli->query("SELECT * FROM idk WHERE login = '$l' and password = '$p'");
+$result = $mysqli->query("SELECT * FROM idk WHERE login = '$l' and password = 'md5($p)'");
 if (mysqli_num_rows($result) > 0){
     echo "veri gud";
 }
