433e1735544e1e2581d61169e7eecf7aeff487a8
Bi6Tasty <janis.rage-ragis@marupe.edu.lv>
2023-10-30 15:17:11 +0200

    more js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function enableButton(){
     let a = document.getElementById('loginName').value 
-    alert(a)
+    let b = document.getElementById('loginPassword').value
+    if (a.length > 0 && b.length > 0){
+        document.getElementById('login').disabled = false
+    }
+    else{
+        document.getElementById('login').disabled = true
+    }
+
+
 }
+
+
+
+
+
+
+
+
